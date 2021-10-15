df1f2985b6b3063416d2b0140bb92525ccaf0843
shaunkeyd <raulbergamo416@gmail.com>
2021-10-14 21:02:50 -0300

    New matches search with team and event name

--- Js/commands/matches.js
+++ Js/commands/matches.js
@@ -35,17 +35,63 @@ module.exports = {
             subcommand
                 .setName('all')
                 .setDescription('return all today next matches!')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('team_name')
+                .setDescription('Search a especific team next matches!')
+                .addStringOption(option => option.setName('team_name').setDescription('Enter team name').setRequired(true))    
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('event_name')
+                .setDescription('Search all the matches for a specific event!')    
+                .addStringOption(option => option.setName('event_name').setDescription('Enter event name').setRequired(true))
         ),
 	async execute(interaction) {
 	
         const matches = await HLTV.getMatches();
 
-        matchesEmbed.fields = []
+        matchesEmbed.fields = [];
+        matchesEmbed.title = 'Partidas de hoje';
+        matchesEmbed.thumbnail.url = 'https://play-lh.googleusercontent.com/chI-8vX4CIbO-pm14tb5UAjip20CwmytQPdz8sBmqNjPqTp1tptpBOZc-aGyYqDesw';
 
         if (interaction.options.getSubcommand() === 'all') {
             matches.forEach(match => createAllMatches(match, matchesEmbed))
 
             await interaction.reply({ embeds: [matchesEmbed] });
+        } else if (interaction.options.getSubcommand() === 'team_name') {
+
+            const team = interaction.options.getString('team_name');
+
+            for (let i = 0; i < matches.length; i++) {
+                if (team.replace(/ /g, '').toUpperCase() === matches[i]['teams'][0]['name'].replace(/ /g, '').toUpperCase() || team.replace(/ /g, '').toUpperCase() === matches[i]['teams'][1]['name'].replace(/ /g, '').toUpperCase()) {
+                    createAllMatches(matches[i], matchesEmbed);
+                    matchesEmbed.thumbnail.url = matches[i]['event']['crest']
+                    if (team.replace(/ /g, '').toUpperCase() === matches[i]['teams'][0]['name'].replace(/ /g, '').toUpperCase()) {
+                        matchesEmbed.title = `Proximas partidas - ${matches[i]['teams'][0]['name']}`
+                    } else {
+                        matchesEmbed.title = `Proximas partidas - ${matches[i]['teams'][1]['name']}`
+                    }
+                } 
+            }
+
+            await interaction.reply({ embeds: [matchesEmbed] });
+
+        } else if (interaction.options.getSubcommand() === 'event_name') {
+
+            const event = interaction.options.getString('event_name');
+
+            for (let i = 0; i < matches.length; i++) {
+                if (event.replace(/ /g, '').toUpperCase() === matches[i]['event']['name'].replace(/ /g, '').toUpperCase()) {
+                    createAllMatches(matches[i], matchesEmbed);
+                    matchesEmbed.thumbnail.url = matches[i]['event']['crest']
+                    matchesEmbed.title = `Proximas partidas para ${matches[i]['event']['name']}`
+                } 
+            }
+
+            await interaction.reply({ embeds: [matchesEmbed] });
+
         }
 	},
 };
